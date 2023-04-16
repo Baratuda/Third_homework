42edd1016ef79e04b5981d02b25005bde20fcef7
Egor Baranovskiy <egorpifagop@gmail.com>
2023-04-16 10:43:19 +0300

    First task was done

--- 0.py
+++ 0.py
@@ -3,7 +3,8 @@ print("Please input array size:")
 array_length = int(input())
 print("Please enter the number you want to calculate in the array.")
 number = int(input())
+
 array = [ra.randrange(1,10) for _ in range(array_length)]
-print(array)
 count = sum([i==number for i in array])  
-print("count")
+
+print(f"count of number({number}) in array: {array} = {count}")
